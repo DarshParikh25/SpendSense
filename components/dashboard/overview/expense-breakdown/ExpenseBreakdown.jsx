24e6ad6420f50fb6a2ee906c3d70a464b3dfe9cc
DarshParikh25 <parikhdarsh121@gmail.com>
2026-02-02 01:09:35 +0530

    Added the dynamic monthly expense pie chart based on the selected account

--- components/dashboard/overview/expense-breakdown/ExpenseBreakdown.jsx
+++ components/dashboard/overview/expense-breakdown/ExpenseBreakdown.jsx
@@ -1,14 +1,34 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+"use client";
+
 import ExpenseChart from "./ExpenseChart";
+import { useAppSelector } from "@/lib/store/hooks/hooks";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const ExpenseBreakdown = () => {
+  const selectedAccount = useAppSelector(
+    (state) => state.ui.recentTransactionsAcc,
+  );
+
   /* Raw data */
   const rawData = [
-    { category: "Rental", value: "1500.00", proportion: "35.56" },
-    { category: "Travel", value: "1251.66", proportion: "29.68" },
-    { category: "Entertainment", value: "304.33", proportion: "7.22" },
-    { category: "Shopping", value: "1161.13", proportion: "27.53" },
+    {
+      account: "personal",
+      allTransactions: [
+        { category: "Rental", value: "1500.00", proportion: "35.56" },
+        { category: "Travel", value: "1251.66", proportion: "29.68" },
+        { category: "Entertainment", value: "304.33", proportion: "7.22" },
+        { category: "Shopping", value: "1161.13", proportion: "27.53" },
+      ],
+    },
+    {
+      account: "work",
+      allTransactions: [],
+    },
   ];
+
+  const rawTransactionData =
+    rawData.find((item) => item.account === selectedAccount)?.allTransactions ??
+    [];
 
   return (
     <Card className="border-2 border-[#bebec0] rounded-xl col-span-1 px-4 py-8">
@@ -18,8 +38,8 @@ const ExpenseBreakdown = () => {
         </CardTitle>
       </CardHeader>
       <CardContent className={"h-full flex justify-center items-center"}>
-        {rawData?.length > 0 ? (
-          <ExpenseChart rawData={rawData} />
+        {rawTransactionData?.length > 0 ? (
+          <ExpenseChart rawData={rawTransactionData} />
         ) : (
           <p className="font-medium">No expenses these months</p>
         )}
